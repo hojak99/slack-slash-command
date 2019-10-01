26de97fd5789d36c4cb38b95c625e96c93d7e5ce
JangHo Kwon <hojak99@gmail.com>
2019-10-01 20:37:14 +0900

    init

---
+++ src/main/java/com/hojak99/slack/SlackSlashCommandApplication.java
@@ -0,0 +1,13 @@
+package com.hojak99.slack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackSlashCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlackSlashCommandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hojak99/slack/SlackSlashCommandApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hojak99.slack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlackSlashCommandApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
